5e5be866d92c58adfc23832afe382f61b23f7019
nishihara <0222natsuki@gmail.com>
2025-06-08 13:22:23 +0900

    ファイルを作成

---
+++ src/test/java/com/rehabmanager/rehab_system/RehabSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rehabmanager.rehab_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RehabSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
